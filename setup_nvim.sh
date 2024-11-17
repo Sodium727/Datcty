6f4e0edbbd3c2f24ceb8de8d83efa6a074da22b3
Sodium <112752505+Sodium727@users.noreply.github.com>
2024-11-17 07:08:16 +0700

    Change "time" to "time -p"

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -193,7 +193,7 @@ g++ -std=c++17 -O2 -Wall -o "$filename" "$1"
 if [ $? -eq 0 ]; then
   echo "Compilation successful. Running $filename..."
   echo "------------------------------"
-  time ./"$filename"
+  time -p ./"$filename"
   echo
   echo "------------------------------"
   rm "$filename"
