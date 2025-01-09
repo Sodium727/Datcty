41d8b355781ae745540a17e7cef8fd55716adbef
Sodium <112752505+Sodium727@users.noreply.github.com>
2025-01-09 12:57:23 +0700

    dosfstools

--- setup_arch.sh
+++ setup_arch.sh
@@ -6,7 +6,7 @@
 sudo pacman -Sy archlinux-keyring --needed
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
-sudo pacman -Syyu --noconfirm --needed git base-devel clang xclip dos2unix ibus-unikey tree neofetch qbittorrent htop gdb ripgrep neovim imv
+sudo pacman -Syyu --noconfirm --needed git base-devel clang xclip dos2unix ibus-unikey tree neofetch qbittorrent htop gdb ripgrep neovim imv dosfstools
 
 git clone https://aur.archlinux.org/yay.git
 
