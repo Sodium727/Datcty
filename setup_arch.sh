f79bcab51f9c76c9de67508b4f76c73216c49638
Sodium <112752505+Sodium727@users.noreply.github.com>
2025-01-12 22:50:03 +0700

    ntfs-3g

--- setup_arch.sh
+++ setup_arch.sh
@@ -6,7 +6,7 @@
 sudo pacman -Sy archlinux-keyring --needed
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
-sudo pacman -Syy --noconfirm --needed git base-devel clang xclip dos2unix ibus-unikey tree fastfetch qbittorrent htop gdb ripgrep neovim imv dosfstools reflector
+sudo pacman -Syy --noconfirm --needed git base-devel clang xclip dos2unix ibus-unikey tree fastfetch qbittorrent htop gdb ripgrep neovim imv dosfstools reflector ntfs-3g
 
 sudo reflector -c Vietnam -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 
