5fd4df48f06b2b2af44b7eb2b4eb4ed07c598371
Sodium <112752505+Sodium727@users.noreply.github.com>
2025-01-13 23:56:20 +0700

    ranger

--- setup_arch.sh
+++ setup_arch.sh
@@ -6,7 +6,7 @@
 sudo pacman -Sy archlinux-keyring --needed
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
-sudo pacman -Syy --noconfirm --needed git base-devel clang xclip dos2unix ibus-unikey tree scrot fastfetch qbittorrent htop gdb ripgrep neovim imv dosfstools reflector ntfs-3g
+sudo pacman -Syy --noconfirm --needed git base-devel clang xclip dos2unix ibus-unikey tree scrot fastfetch qbittorrent htop gdb ripgrep neovim imv dosfstools reflector ntfs-3g ranger
 
 sudo reflector -c Vietnam -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 
@@ -294,3 +294,4 @@ echo "A few more things to do: if you are using i3, paste this into the ~/.confi
 echo "exec --no-startup-id xset r rate 200 60"
 echo "exec --no-startup-id ibus-daemon"
 echo "bindsym $mod+Shift+s exec scrot -s - | xclip -selection clipboard -t image/png"
+echo "bindsym $mod+Shift+Return exec i3-sensible-terminal -e ranger"
