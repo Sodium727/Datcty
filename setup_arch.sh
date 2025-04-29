97fe0a443f23d034ae87e8eae2a1bb2966924b68
Sodium727 <tandatpham2608@gmail.com>
2025-04-29 19:49:57 +0700

    Added custom sddm config (forked from eucalyptus-drop)

--- setup_arch.sh
+++ setup_arch.sh
@@ -9,14 +9,14 @@
 # sudo pacman-key --populate archlinux
 
 # Essentials
-sudo pacman -Sy --noconfirm --needed git base-devel clang xclip dos2unix tree fastfetch qbittorrent htop gdb ripgrep neovim imv dosfstools ntfs-3g bat exa zoxide fcitx5 fcitx5-unikey fcitx5-qt fcitx5-gtk fcitx5-configtool 
+sudo pacman -Sy --noconfirm --needed git base-devel clang xclip dos2unix tree fastfetch qbittorrent htop gdb ripgrep neovim imv dosfstools ntfs-3g bat exa zoxide fcitx5 fcitx5-unikey fcitx5-qt fcitx5-gtk fcitx5-configtool mpv
 
 # For using Wine (and other stuffs)
 # sudo pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader wine-staging winetricks vulkan-headers lib32-mesa lib32-vulkan-icd-loader mesa vulkan-intel gnutls lib32-gnutls vulkan-intel lib32-vulkan-intel wine-mono noto-fonts-cjk noto-fonts-emoji noto-fonts-extra noto-fonts
 
 # winetricks d3dcompiler_47 d3d9 d3d11 dxvk vulkan
 
-# paru -S --noconfirm --needed ttf-jetbrains-mono-nerd flashplayer-standalone
+paru -S --noconfirm --needed ttf-jetbrains-mono-nerd flashplayer-standalone
 
 # Install Packer plugin manager for Neovim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
@@ -293,10 +293,10 @@ EOL
 nvim +PackerInstall
 
 # Setup Hyprland + Preconfigured setup
-# git clone --depth 1 https://github.com/JaKooLit/Arch-Hyprland.git ~/Arch-Hyprland
-# cd ~/Arch-Hyprland
-# chmod +x install.sh
-# ./install.sh
+git clone https://github.com/JaKooLit/Arch-Hyprland.git ~/Arch-Hyprland
+cd ~/Arch-Hyprland
+chmod +x install.sh
+./install.sh
 
 echo "Setup complete!"
 
