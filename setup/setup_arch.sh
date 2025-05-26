e9e3a65efcc497acc6583b213f77c70749c29e19
Sodium727 <tandatpham2608@gmail.com>
2025-05-26 21:40:58 +0700

    Changes to zshrc, hypr, waybar

--- setup/setup_arch.sh
+++ setup/setup_arch.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 # This bash script is meant to be fetched from https://github.com/Sodium727/Datcty, packed with everything included in the repo
 # It automates the post-installation of Arch Linux
 
@@ -7,32 +8,27 @@
 # sudo pacman-key --init
 # sudo pacman-key --populate archlinux
 
-# Essentials
-sudo pacman -Sy --noconfirm --needed git base-devel clang xclip dos2unix fastfetch qbittorrent gdb ripgrep neovim imv dosfstools ntfs-3g bat eza zoxide fcitx5 fcitx5-unikey fcitx5-qt fcitx5-gtk fcitx5-configtool mpv zsh kitty sddm firefox cliphist
-
-# For using Wine (and other stuffs)
-# sudo pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader wine-staging winetricks vulkan-headers lib32-mesa lib32-vulkan-icd-loader mesa vulkan-intel gnutls lib32-gnutls vulkan-intel lib32-vulkan-intel wine-mono noto-fonts-cjk noto-fonts-emoji noto-fonts-extra noto-fonts
-
-# winetricks d3dcompiler_47 d3d9 d3d11 dxvk vulkan
+# Packages
+sudo pacman -Sy --noconfirm --needed git base-devel clang wl-clipboard less qbittorrent gdb ripgrep neovim imv bat eza zoxide fcitx5 fcitx5-unikey fcitx5-config-qt mpv zsh kitty sddm firefox cliphist python-pynvim tmux git-zsh-completion libnotify flatpak ttf-jetbrains-mono-nerd hyprlock waybar hyprland hyprpaper hyprpolkitagent hyprsunset hyprpicker 7zip
 
 # Install yay
-git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+git clone https://aur.archlinux.org/yay-bin.git && cd ~/yay-bin && makepkg -si
 
-# Install font and other stuffs
-yay -S --noconfirm --needed ttf-jetbrains-mono-nerd flashplayer-standalone
+# other Hyprland packages from AUR
+yay -S --noconfirm --needed hyprshot waypaper wlogout
 
 # Install Packer plugin manager for Neovim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-# Then install neovim, pull the nvim/ from configs/ to ~/.config and run :PackerSync in neovim
+# pull the nvim/ from configs/ to ~/.config and run :PackerSync in neovim
 
-# Setup Hyprland + Preconfigured setup (ABANDONED.)
+# JaKooLit Hyprland Setup
 # git clone https://github.com/JaKooLit/Arch-Hyprland.git ~/Arch-Hyprland
 # cd ~/Arch-Hyprland
 # chmod +x install.sh
 # ./install.sh
 
-# end-4's illogical impulse (Won't use it unless feeling fancy)
+# end-4's illogical impulse
 # bash <(curl -s "https://end-4.github.io/dots-hyprland-wiki/setup.sh")
 
 # Zsh Setup
@@ -41,11 +37,11 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+cat <<EOF
+Remember to copy everything from configs/zshrc to ~/.zshrc
+EOF
 
-# Hyprland setup
-yay -S --needed hyprcursor hyprgraphics hyprland hyprland-qt-support-git hyprland-qt-support-git-debug hyprland-qtutils hyprlang-git hyprlang-git-debug hyprpaper hyprpolkitagent hyprshot hyprsunset hyprutils-git hyprutils-git-debug hyprwayland-scanner-git hyprwayland-scanner-git-debug --noconfirm
-
-# Extras
+# Stuffs
 cat <<EOF
 Add the stuff from configs/ (except rofi-beats and zshrc) to ~/.config and it should be ok.
 
@@ -74,3 +70,8 @@ Include = /etc/pacman.d/mirrorlist
 SigLevel = Never
 Server = https://cdn-mirror.chaotic.cx/chaotic-aur/$arch
 EOF
+
+# For using Wine (and other stuffs)
+# sudo pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader wine-staging winetricks vulkan-headers lib32-mesa lib32-vulkan-icd-loader mesa vulkan-intel gnutls lib32-gnutls vulkan-intel lib32-vulkan-intel wine-mono noto-fonts-cjk noto-fonts-emoji noto-fonts-extra noto-fonts
+
+# winetricks d3dcompiler_47 d3d9 d3d11 dxvk vulkan
