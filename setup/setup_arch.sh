e42d01bb9e5bcd16fe9a3830b4b1adc95367474b
Sodium727 <tandatpham2608@gmail.com>
2025-06-13 10:02:10 +0700

    Add labwc/

--- setup/setup_arch.sh
+++ setup/setup_arch.sh
@@ -8,17 +8,23 @@
 # sudo pacman-key --init
 # sudo pacman-key --populate archlinux
 
-# Packages
-sudo pacman -Sy --noconfirm --needed git base-devel clang wl-clipboard less qbittorrent gdb ripgrep neovim imv bat eza zoxide fcitx5 fcitx5-unikey fcitx5-config-qt mpv zsh kitty sddm firefox cliphist python-pynvim tmux git-zsh-completion libnotify flatpak ttf-jetbrains-mono-nerd hyprlock waybar hyprland hyprpaper hyprpolkitagent hyprsunset hyprpicker 7zip
+# Packages (general)
+sudo pacman -Sy --noconfirm --needed git base-devel clang wl-clipboard less qbittorrent gdb ripgrep neovim imv bat eza zoxide fcitx5 fcitx5-unikey fcitx5-config-qt mpv zsh kitty sddm firefox cliphist python-pynvim tmux libnotify flatpak ttf-jetbrains-mono-nerd 7zip waybar
+
+# Packages (Hyprland)
+# sudo pacman -S --noconfirm --needed hyprlock hyprland hyprpaper hyprpolkitagent hyprsunset hyprpicker
 
 # Install yay
 git clone https://aur.archlinux.org/yay-bin.git && cd ~/yay-bin && makepkg -si
 
-# other Hyprland packages from AUR
-yay -S --noconfirm --needed hyprshot waypaper wlogout
+# AUR Packages
+yay -S --noconfirm --needed waypaper wlogout
+
+# AUR Packages (Hyprland)
+# yay -S --noconfirm --needed hyprshot 
 
 # Install Packer plugin manager for Neovim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # pull the nvim/ from configs/ to ~/.config and run :PackerSync in neovim
 
@@ -36,7 +42,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
+ 
 cat <<EOF
 Remember to copy everything from configs/zshrc to ~/.zshrc
 EOF
@@ -71,7 +77,7 @@ SigLevel = Never
 Server = https://cdn-mirror.chaotic.cx/chaotic-aur/$arch
 EOF
 
-# For using Wine (and other stuffs)
-# sudo pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader wine-staging winetricks vulkan-headers lib32-mesa lib32-vulkan-icd-loader mesa vulkan-intel gnutls lib32-gnutls vulkan-intel lib32-vulkan-intel wine-mono noto-fonts-cjk noto-fonts-emoji noto-fonts-extra noto-fonts
+# Wine (and other driver stuffs)
+# sudo pacman -Sy --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader wine-staging winetricks vulkan-headers lib32-mesa lib32-vulkan-icd-loader mesa vulkan-intel gnutls lib32-gnutls vulkan-intel lib32-vulkan-intel wine-mono
 
 # winetricks d3dcompiler_47 d3d9 d3d11 dxvk vulkan
